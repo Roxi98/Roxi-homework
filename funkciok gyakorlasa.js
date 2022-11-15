48ad042b95e2df6b7fe55f6c3a1e026cca367c70
Roxi98 <116589716+Roxi98@users.noreply.github.com>
2022-11-15 12:56:21 +0100

    mau

--- funkciok gyakorlasa.js
+++ funkciok gyakorlasa.js
@@ -7,7 +7,7 @@ function osszeadas(elso, masodik, harmadik, negyedik) {
   return elso + masodik + harmadik + negyedik;
 }
 console.log(osszeadas(a, b, c, d));*/
-const lista = ["pizza", "suti", "becsi", "porkolt"];
+/*const lista = ["pizza", "suti", "becsi", "porkolt"];
 const szoveg = "A kedvenc kajaim a ";
 function finom(kaja) {
   let szoveg = "A kedvenc kajaim a ";
@@ -16,4 +16,21 @@ function finom(kaja) {
   }
   return szoveg;
 }
-console.log(finom(lista));
+console.log(finom(lista));*/
+const mau = [];
+const vau = [
+  {
+    nev: "hola",
+    kor: 2,
+  },
+  {
+    nev: "mola",
+    kor: 2,
+  },
+];
+function allat(cica, kutya) {
+  for (let i = 0; i < kutya.length; i++) cica[kutya[i].nev] = kutya[i].kor;
+  return kutya;
+}
+console.log(allat(mau, vau));
+console.log(mau);
