58c52395fd86f58078b12918fae7019fb3d8574a
Roxi98 <116589716+Roxi98@users.noreply.github.com>
2022-11-21 18:00:46 +0100

    mau

--- test.js
+++ test.js
@@ -8,31 +8,28 @@ function randomIntBetweenTwoNumbers(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
 const N = 20;
 kocka20 = Array.from({ length: N }, (_, index) => index + 1);
 const N1 = 8;
 kocka8 = Array.from({ length: N1 }, (_, index) => index + 1);
 const N2 = 10;
 kocka10 = Array.from({ length: N2 }, (_, index) => index + 1);
-
-function roll2(dye1, dice2, dice3) {
-  return (kockakdobasa =
-    randomIntBetweenTwoNumbers(0, 19) +
-    randomIntBetweenTwoNumbers(0, 7) +
-    randomIntBetweenTwoNumbers(0, 9));
+let osszkockak = [kocka20, kocka8, kocka10, kocka20];
+function vegtelen(mau) {
+  let kockaoldalak = [];
+  for (let i = 0; i < osszkockak.length; i++) {
+    return kockaoldalak.push(osszkockak.length);
+  }
+  console.log(kockaoldalak);
 }
-console.log(
-  "20,8 es 10 oldalu kocka osszege : " + roll2(kocka20, kocka8, kocka10)
-);
+console.log(vegtelen(osszkockak));
 
-function roll(dicee, darabszam) {
-  for (let i = 0; i < dicee.length; i++) {
-    const kockadobas = randomIntBetweenTwoNumbers(
-      dicee[0] * darabszam,
-      dicee[5] * darabszam
-    );
-
-    return kockadobas;
+function osszkocka(allDice) {
+  for (let i = 0; i < allDice.length; i++) {
+    const vegtelenkocka = allDice[i];
+    return vegtelenkocka;
   }
 }
-console.log("x mennyisegu kocka osszege: " + roll(kocka, 10));
+console.log(osszkocka(osszkockak));
+function Roll(mindenkocka) {}
