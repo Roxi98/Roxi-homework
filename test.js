c4568ba7da15457a3268769f3817db2fa7d86fe1
Roxi98 <116589716+Roxi98@users.noreply.github.com>
2022-11-21 22:07:16 +0100

    hazi

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const kocka = [1, 2, 3, 4, 5, 6];
 let kocka20 = [];
 let kocka8 = [];
 let kocka10 = [];
+let kockaString = ["Boris", "Jimmy", "Roxi", "lol", "gugya", "szilya"];
+console.log(kockaString);
 
 function randomIntBetweenTwoNumbers(min, max) {
   min = Math.ceil(min);
@@ -15,19 +17,21 @@ const N1 = 8;
 kocka8 = Array.from({ length: N1 }, (_, index) => index + 1);
 const N2 = 10;
 kocka10 = Array.from({ length: N2 }, (_, index) => index + 1);
-let osszkockak = [kocka20, kocka8, kocka10, kocka20];
 
-function osszkocka(allDice) {
-  for (let i = 0; i < allDice.length; i++) {
-    const vegtelenkocka = allDice[i];
-    return vegtelenkocka;
+function Roll(dice) {
+  const mau = [];
+  for (let i = 0; i < dice.length; i++) {
+    const kockaszam = [];
+    kockaszam.push(randomIntBetweenTwoNumbers(0, dice[i].length - 1));
+    console.log("kockaszam", kockaszam);
+    console.log(dice[i][kockaszam]);
+    if (typeof dice[i][kockaszam] === "string") {
+      mau.push(dice[i][kockaszam]);
+    } else {
+      mau.push(dice[i][kockaszam]);
+    }
   }
+  return mau;
 }
-console.log(osszkocka(osszkockak));
-function Roll(vegtelenkocka) {
-  return randomIntBetweenTwoNumbers(
-    allDice[i][0],
-    allDice[i][allDice.lenght - 1]
-  );
-}
-console.log(Roll([kocka]));
+
+console.log(Roll([kocka20, kocka8, kocka10, kockaString]));
