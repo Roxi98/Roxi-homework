5336a7f5a53f0ce5157809aec0e75018399bc60a
Roxi98 <116589716+Roxi98@users.noreply.github.com>
2022-11-22 19:50:06 +0100

    mau

--- BorisfeladatNo2.js
+++ BorisfeladatNo2.js
@@ -3,6 +3,7 @@ const idemoLista = [
   [1, 888, 434, 21312],
   [1, 324, 277, 563],
   [555, 666, 777, 667],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 500000],
 ];
 function biggest(number) {
   console.log(number);
